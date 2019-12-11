d1e7b9ccee75ba3506d21a28ae632474fc4eabc1
s-l-w <2920434096@qq.com>
2019-12-11 23:49:28 +0800

    原始.c

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main ()
+{int num(char);
+int i,k,(*p)(char a[]);
+char b[100];
+    gets(b);
+p=num;
+k=(*p)(b);
+printf("%d",k);
+return 0;
+}
+int num(char)
+{int n,l;
+for(n=0;a[n]!=0;n++)
+{
+    l=n+1;
+}
+return(l);}
